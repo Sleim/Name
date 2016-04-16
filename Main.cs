36576cae2c6c6c9e3c22447f3fe206955d8f7565
Sleim <dronk01@gmail.com>
2016-04-17 00:16:32 +0300

    KR

---
+++ Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace KrRob
+{
+    class Date
+    {
+        List<FileStream> File1 = new List<FileStream>();   //Создаем наш файловый "список"
+        FileStream file;
+        public void Create()  
+        {
+            file = File.Create("Файл1.txt");   //Создаем новый текстовый файл
+            File1.Add(file);
+        }
+        public void Delete()
+        {
+            File1.Remove(file);      //Удаляем файл
+        }
+        public void Read()
+        {
+            using (FileStream file = File.OpenRead("Файл1.txt"))  //Открываем наш файл
+            {
+                byte[] File = new byte[1024];
+                UTF8Encoding sample = new UTF8Encoding(true);
+                while (file.Read(File, 0, File.Length) > 0)
+                {
+                    Console.WriteLine(sample.GetString(File));
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrRob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AndrikUser Andrik = new AndrikUser();  
+            Andrik.Enter();   //Запускаем вход в нашего пользователя
+            Console.ReadKey();
+        }
+    }
+}
